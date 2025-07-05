3c9facbbfc78ad48e6a24cdbd5aabb2be26487d8
gil100-dev <gilsol08@gmail.com>
2025-07-05 13:47:26 +0300

    Convert to binary

--- Cyber-curse.py
+++ Cyber-curse.py
@@ -1,6 +1,8 @@
 def binary_to_decimal(binary_str):
     return int(binary_str, 2)
 
+
+
 def number_to_digit_array(number):
     return [int(d) for d in str(number)]
 
@@ -11,14 +13,23 @@ def count_digits_recursive(arr, digit, idx=0, count=0):
         count += 1
     return count_digits_recursive(arr, digit, idx + 1, count)
 
+def decimal_to_binary(n, number_sibiut):
+    if n >= 0:
+        return format(n, f'0{number_sibiut}b')
+    else:
+        return format((1 << number_sibiut) + n, f'0{number_sibiut}b')
+
 def main():
-    binary_input = input("Enter a binary number: ")
-    decimal_number = binary_to_decimal(binary_input)
-    digits_array = number_to_digit_array(decimal_number)
-    print("Array:", digits_array)
-    for d in range(10):
-        count = count_digits_recursive(digits_array, d)
-        print(f"Digit {d} appears {count} times")
+    decimal_input = int(input("Enter a decimal number: "))
+    number_sibiut = int(input("Enter number of bits: "))
+
+    binary_twos_complement = decimal_to_binary(decimal_input, number_sibiut)
+
+    print(f"Two's complement ({number_sibiut}-bit): {binary_twos_complement}")
+
+if __name__ == "__main__":
+    main()
+
 
 if __name__ == "__main__":
     main()
